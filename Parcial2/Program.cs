9ce743072619a428f4957cfe0b26d7535408aa98
Daniel Andre Elis Galvez <delisg@miumg.edu.gt>
2026-05-02 16:40:13 -0600

    fix docker para deploy

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -9,11 +9,33 @@ using Parcial2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Render injects DATABASE_URL in URI format; Npgsql needs key-value format
+static string ResolveConnectionString(IConfiguration config)
+{
+    var raw = config.GetConnectionString("DefaultConnection")
+              ?? Environment.GetEnvironmentVariable("DATABASE_URL")
+              ?? "";
+
+    if (!raw.StartsWith("postgres://") && !raw.StartsWith("postgresql://"))
+        return raw;
+
+    var uri  = new Uri(raw);
+    var info = uri.UserInfo.Split(':');
+    var host = uri.Host;
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    var db   = uri.AbsolutePath.TrimStart('/');
+    var user = Uri.UnescapeDataString(info[0]);
+    var pass = info.Length > 1 ? Uri.UnescapeDataString(info[1]) : "";
+
+    return $"Host={host};Port={port};Database={db};Username={user};Password={pass};SSL Mode=Require;Trust Server Certificate=true;";
+}
+
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 
+var connStr = ResolveConnectionString(builder.Configuration);
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connStr));
 
 var jwtKey = builder.Configuration["Jwt:Key"]!;
 builder.Services.AddAuthentication(options =>
